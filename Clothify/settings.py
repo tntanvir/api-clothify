258162c9d3ad13daef798d37448fb5ff824071f0
Tanvir <tntanvir2382018@gmail.com>
2024-09-01 23:41:15 +0600

    ok

--- Clothify/settings.py
+++ Clothify/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ["https://api-clothify.onrender.com/","https://*.127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://api-clothify.onrender.com","https://*.127.0.0.1"]
 
 
 # Application definition
